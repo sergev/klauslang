b278b97b83cde3e13e3877797c6ab3faac7ae087
Константин Захаров <czaerlag@yandex.ru>
2024-12-27 00:57:42 +0300

    build-alt

---
+++ installer/build-alt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Version number must be passed in $1
+# Compiled binaries must exist in ../compiled
+
+set -e
+set -u
+
+VER="$1"
+
+rpmbuild -bb --define "_ver $VER" --define "_pwd $(pwd)" klauslang-alt.spec
