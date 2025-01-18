0651a74c5611fd1b703cb07f6414872ff668de51
Константин Захаров <czaerlag@yandex.ru>
2025-01-18 19:43:42 +0300

    v2.5.8

--- installer/build-alt.sh
+++ installer/build-alt.sh
@@ -2,11 +2,14 @@
 #
 # Requires: ALT Linux (>= 10.4)
 #
-# This will create a source tarball and build RPM packages for ALT Linux.
+# This will create the source tarball and build RPM packages for ALT Linux.
 # rpm --eval %{_topdir} must return a sane RPM top directory.
 #
 # Make sure you have checked out the correct branch (e.g. git checkout v2.5.8)
 #
+# Since v2.5.8 the source tarball can be downloaded at
+# https://gitflic.ru/project/czaerlag/klauslang/file/downloadAll?format=tar.bz2&branch=v<VER>
+# (replace <VER> with the actual version number)
 
 set -e
 set -u
